66eaced89289e060bd9c7541f1b654b9341038b4
Bhavesh Negi <bhaveshnegi98@gmail.com>
2024-12-10 14:17:23 +0530

    protfolio

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Bhavesh Negi</h1>
+        <p className={styles.description}>
+        As a fresher, I aim to develop and contribute towards organizational goals while enhancing my
+        professional skill.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/profile.svg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
